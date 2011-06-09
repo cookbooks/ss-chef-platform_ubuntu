a1ac91e41e2c3650656be92fb310b02f7dad1afe
Fletcher Nichol <fnichol@nichol.ca>
2011-06-08 21:33:48 -0600

    Update readme link in metadata.

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer       "Example Com"
 maintainer_email "ops@example.com"
 license          "Apache 2.0"
 description      "Installs/Configures platform_ubuntu"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
